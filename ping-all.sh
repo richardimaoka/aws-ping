67b5eb5496c37617b93b181b88dff7bd43875702
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-05-02 23:20:27 +0900

    ping

--- ping-all.sh
+++ ping-all.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
 STACK_NAME="PingExperiment"
+LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
+
 
 for region in $(aws ec2 describe-regions --query "Regions[].RegionName" | jq -r '.[]')
 do
+  echo "------------------------------------------------------------"
+  echo "Ping experiment for ${region}"
+  echo "------------------------------------------------------------"
+
   EC2_INSTANCES=$(aws ec2 describe-instances \
     --filters "Name=tag:aws:cloudformation:stack-name,Values=${STACK_NAME}" \
     --query "Reservations[].Instances[]" \
-    --output json
     --region ${region}
   )
 
-  for instance in $(echo ${EC2_INSTANCES} | jq -c '.')
+  for instance in $(echo ${EC2_INSTANCES} | jq -c '.[]')
   do
     AVAILABILITY_ZONE=$(echo "${instance}" | jq -r '.Placement.AvailabilityZone')
-    LOCAL_IP_ADDRESS=$(echo "${instance}" | jq -r '.PrivateIpAddress')
+    TARGETIP__ADDRESS=$(echo "${instance}" | jq -r '.PrivateIpAddress')
 
-    if [ -n "${AVAILABILITY_ZONE}" ] && [ -n "${LOCAL_IP_ADDRESS}" ]; then
-      echo "Pinging an instance in ${AVAILABILITY_ZONE}"
-      ping -c 30 "${LOCAL_IP_ADDRESS}"
+    if [ -n "${AVAILABILITY_ZONE}" ] && [ -n "${TARGET_IP_ADDRESS}" ]; then
+      echo "Pinging an instance in ${AVAILABILITY_ZONE} from ${LOCAL_IPV4}"
+      ping -c 30 "${TARGET_IP_ADDRESS}"
     fi
   done
 done
