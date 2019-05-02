bde20ed3a3136fb5045830702716896391ac8f0e
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-05-02 23:03:49 +0900

    ping

--- ping-all.sh
+++ ping-all.sh
@@ -2,14 +2,23 @@
 
 STACK_NAME="PingExperiment"
 
-for region in $(aws ec2 describe-regions --query "Regions[].RegionName" | jq)
+for region in $(aws ec2 describe-regions --query "Regions[].RegionName" | jq -r '.[]')
 do
   EC2_INSTANCES=$(aws ec2 describe-instances \
     --filters "Name=tag:aws:cloudformation:stack-name,Values=${STACK_NAME}" \
+    --query "Reservations[].Instances[]" \
+    --output json
     --region ${region}
   )
+
   for instance in $(echo ${EC2_INSTANCES} | jq -c '.')
   do
-    echo "$instance"
+    AVAILABILITY_ZONE=$(echo "${instance}" | jq -r '.Placement.AvailabilityZone')
+    LOCAL_IP_ADDRESS=$(echo "${instance}" | jq -r '.PrivateIpAddress')
+
+    if [ -n "${AVAILABILITY_ZONE}" ] && [ -n "${LOCAL_IP_ADDRESS}" ]; then
+      echo "Pinging an instance in ${AVAILABILITY_ZONE}"
+      ping -c 30 "${LOCAL_IP_ADDRESS}"
+    fi
   done
 done
