b7798fbd46bb9a164c819d7fa035d8873b58706a
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-05-02 14:30:36 +0900

    Parameters in the shell script

--- shellscript.sh
+++ shellscript.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-# Any subsequent(*) commands which fail will cause the shell script to exit immediately
-set -e
-
 AWS_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')" \
 SSH_LOCATION="$(curl ifconfig.co 2> /dev/null)/32"
 STACK_NAME_VPC_MAIN="PingMainVPC"
 
-echo "creating the main CloudFormation stack"
-aws cloudformation create-stack \
-  --stack-name "${STACK_NAME_VPC_MAIN}" \
-  --template-body file://cloudformation.yaml \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameters ParameterKey=SSHLocation,ParameterValue="${SSH_LOCATION}" \
-               ParameterKey=PeerRequesterAccountId,ParameterValue="${AWS_ACCOUNT_ID}"
+STACK_EXISTS=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME_VPC_MAIN}")
+
+if [ -z "${STACK_EXISTS}" ]; then
+  echo "creating the main CloudFormation stack"
+  aws cloudformation create-stack \
+    --stack-name "${STACK_NAME_VPC_MAIN}" \
+    --template-body file://cloudformation.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters ParameterKey=SSHLocation,ParameterValue="${SSH_LOCATION}" \
+                 ParameterKey=AWSAccountIdForMainVPC,ParameterValue="${AWS_ACCOUNT_ID}"
+fi
 
 echo "Waiting until the Cloudformation VPC main stack is CREATE_COMPLETE"
 aws cloudformation wait stack-create-complete --stack-name "${STACK_NAME_VPC_MAIN}"
+
+aws cloudformation describe-stacks --stack-name "${STACK_NAME_VPC_MAIN}" 
+MAIN_VPC_ID=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME_VPC_MAIN}" --query "Stacks[].Outputs[?OutputKey=='VPCId'].OutputValue" --output text)
+PEER_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME_VPC_MAIN}" --query "Stacks[].Outputs[?OutputKey=='PeerRoleARN'].OutputValue" --output text)
+
 
 echo "creating a sub CloudFormation stack"
 aws cloudformation create-stack \
@@ -24,5 +30,7 @@ aws cloudformation create-stack \
   --template-body file://cloudformation.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters ParameterKey=SSHLocation,ParameterValue="${SSH_LOCATION}" \
-               ParameterKey=PeerRequesterAccountId,ParameterValue="${AWS_ACCOUNT_ID}" \
+               ParameterKey=AWSAccountIdForMainVPC,ParameterValue="${AWS_ACCOUNT_ID}" \
+               ParameterKey=PeerVpcId,ParameterValue="${MAIN_VPC_ID}" \
+               ParameterKey=PeerRoleARN,ParameterValue="${PEER_ROLE_ARN}" \
   --region "us-east-1"               
