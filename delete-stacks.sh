0aa6b9408f86433900fe1b3b90bc8a43f6b06f66
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-05-02 23:30:24 +0900

    delete stack

---
+++ delete-stacks.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+STACK_NAME="PingExperiment"
+
+for region in $(aws ec2 describe-regions --query "Regions[].RegionName" | jq -r '.[]')
+do 
+  echo "Deleting the CloudFormatoin stack=${STACK_NAME} for region=${region} if exists."
+  aws cloudformation delete-stack --stack-name "${STACK_NAME}" --region "${region}"
+done 
