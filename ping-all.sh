6fbc8e5f366e7726f6277048ec39bf86e4a12ab4
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-05-09 01:06:01 +0900

    region handling

--- ping-all.sh
+++ ping-all.sh
@@ -2,28 +2,51 @@
 
 STACK_NAME="PingExperiment"
 LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
-DEFAULT_REGION=$(aws configure get region)
 
-for region in $(aws ec2 describe-regions --query "Regions[].RegionName" --region "${DEFAULT_REGION}"| jq -r '.[]')
+for OPT in "$@"
 do
-  echo "------------------------------------------------------------"
-  echo "Ping experiment for ${region}"
-  echo "------------------------------------------------------------"
+    case "$OPT" in
+        '--region' )
+            if [ -z "$2" ]; then
+                echo "option --region requires an argument -- $1" 1>&2
+                exit 1
+            fi
+            CURRENT_REGION="$2"
+            shift 2
+            ;;
+        -*)
+            echo "illegal option -- $1" 1>&2
+            exit 1
+            ;;
 
-  EC2_INSTANCES=$(aws ec2 describe-instances \
-    --filters "Name=tag:aws:cloudformation:stack-name,Values=${STACK_NAME}" \
-    --query "Reservations[].Instances[]" \
-    --region "${region}"
-  )
-
-  for instance in $(echo "${EC2_INSTANCES}" | jq -c '.[]')
-  do
-    AVAILABILITY_ZONE=$(echo "${instance}" | jq -r '.Placement.AvailabilityZone')
-    TARGET_IP_ADDRESS=$(echo "${instance}" | jq -r '.PrivateIpAddress')
-
-    if [ -n "${AVAILABILITY_ZONE}" ] && [ -n "${TARGET_IP_ADDRESS}" ]; then
-      echo "Pinging an instance in ${AVAILABILITY_ZONE} from ${LOCAL_IPV4}"
-      ping -c 30 "${TARGET_IP_ADDRESS}"
-    fi
-  done
+    esac
 done
+
+if [ -n "${CURRENT_REGION}" ]; then
+  for region in $(aws ec2 describe-regions --query "Regions[].RegionName" --region "${CURRENT_REGION}" | jq -r '.[]')
+  do
+    echo "------------------------------------------------------------"
+    echo "Ping experiment for ${region}"
+    echo "------------------------------------------------------------"
+
+    EC2_INSTANCES=$(aws ec2 describe-instances \
+      --filters "Name=tag:aws:cloudformation:stack-name,Values=${STACK_NAME}" \
+      --query "Reservations[].Instances[]" \
+      --region "${region}"
+    )
+
+    for instance in $(echo "${EC2_INSTANCES}" | jq -c '.[]')
+    do
+      AVAILABILITY_ZONE=$(echo "${instance}" | jq -r '.Placement.AvailabilityZone')
+      TARGET_IP_ADDRESS=$(echo "${instance}" | jq -r '.PrivateIpAddress')
+
+      if [ -n "${AVAILABILITY_ZONE}" ] && [ -n "${TARGET_IP_ADDRESS}" ]; then
+        echo "Pinging an instance in ${AVAILABILITY_ZONE} from ${LOCAL_IPV4}"
+        ping -c 30 "${TARGET_IP_ADDRESS}"
+      fi
+    done
+  done
+else
+  echo "--region option must be passed but was empty"
+fi
+
--- send-command.sh
+++ send-command.sh
@@ -12,6 +12,6 @@ aws ssm send-command \
   --instance-ids "${PING_ORIGIN_EC2}" \
   --document-name "AWS-RunShellScript" \
   --comment "aws-ping command to run ping to all relevant EC2 instances in all the regions" \
-  --parameters commands=["/home/ec2-user/aws-ping/ping-all.sh"] \
+  --parameters commands=["/home/ec2-user/aws-ping/ping-all.sh --region ${DEFAULT_REGION}"] \
   --output text \
   --query "Command.CommandIad"
