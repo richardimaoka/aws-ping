f5e58e8077e7298aace3d2cc97446d34da848010
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-05-08 11:15:54 +0900

    update

--- create-stacks.sh
+++ create-stacks.sh
@@ -7,7 +7,7 @@ STACK_NAME="PingExperiment"
 STACK_EXISTS=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" 2>/dev/null)
 
 if [ -z "${STACK_EXISTS}" ]; then
-  echo "creating the main CloudFormation stack"
+  echo "creating the main CloudFormation stack for $(aws configure get region)"
   aws cloudformation create-stack \
     --stack-name "${STACK_NAME}" \
     --template-body file://cloudformation.yaml \
@@ -26,26 +26,35 @@ DEFAULT_REGION=$(aws configure get region)
 
 for region in $(aws ec2 describe-regions --query "Regions[].RegionName" | jq -r '.[]')
 do 
-  echo "Creating a CloudFormation stack=${STACK_NAME} for region=${region}"
-  aws cloudformation create-stack \
-    --stack-name "${STACK_NAME}" \
-    --template-body file://cloudformation.yaml \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameters ParameterKey=SSHLocation,ParameterValue="${SSH_LOCATION}" \
-                ParameterKey=AWSAccountIdForMainVPC,ParameterValue="${AWS_ACCOUNT_ID}" \
-                ParameterKey=PeerVpcId,ParameterValue="${MAIN_VPC_ID}" \
-                ParameterKey=PeerRoleArn,ParameterValue="${PEER_ROLE_ARN}" \
-                ParameterKey=PeerRegion,ParameterValue="${DEFAULT_REGION}" \
-    --region "${region}"
+  SUB_STACK_EXISTS=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --region "${region}" 2>/dev/null)
+  if [ -z "${SUB_STACK_EXISTS}" ]; then
+    echo "Creating a CloudFormation stack=${STACK_NAME} for region=${region}"
+    aws cloudformation create-stack \
+      --stack-name "${STACK_NAME}" \
+      --template-body file://cloudformation.yaml \
+      --capabilities CAPABILITY_NAMED_IAM \
+      --parameters ParameterKey=SSHLocation,ParameterValue="${SSH_LOCATION}" \
+                   ParameterKey=AWSAccountIdForMainVPC,ParameterValue="${AWS_ACCOUNT_ID}" \
+                   ParameterKey=PeerVpcId,ParameterValue="${MAIN_VPC_ID}" \
+                   ParameterKey=PeerRoleArn,ParameterValue="${PEER_ROLE_ARN}" \
+                   ParameterKey=PeerRegion,ParameterValue="${DEFAULT_REGION}" \
+      --region "${region}"
+  fi
+
+  echo "Waiting until the Cloudformation stack is CREATE_COMPLETE for ${region}"
+  aws cloudformation wait stack-create-complete --stack-name "${STACK_NAME}" --region "${region}"
 
   # Doing this in CloudFormation is pretty tediuos as described in README.md, so doing it in AWS CLI
   echo "Adding VPC peering route to the main VPC's route table"
-  VPC_PEERING_CONNECTION=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query "Stacks[].Outputs[?OutputKey=='PeerRoleArn'].OutputValue" --output text)
-  VPC_CIDR_BLOCK=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query "Stacks[].Outputs[?OutputKey=='VPCCidrBlock'].OutputValue" --output text)
+  VPC_PEERING_CONNECTION=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query "Stacks[].Outputs[?OutputKey=='VPCPeeringConnection'].OutputValue" --output text --region "${region}")
+  VPC_CIDR_BLOCK=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query "Stacks[].Outputs[?OutputKey=='VPCCidrBlock'].OutputValue" --output text --region "${region}")
+  echo "${MAIN_ROUTE_TABLE}"
+  echo "${VPC_CIDR_BLOCK}"
+  echo "${VPC_PEERING_CONNECTION}"
   aws ec2 create-route \
-    --route-table-id ${MAIN_ROUTE_TABLE} \
-    --destination-cidr-block ${VPC_CIDR_BLOCK} \
-    --vpc-peering-connection-id ${VPC_PEERING_CONNECTION}
+    --route-table-id "${MAIN_ROUTE_TABLE}" \
+    --destination-cidr-block "${VPC_CIDR_BLOCK}" \
+    --vpc-peering-connection-id "${VPC_PEERING_CONNECTION}"
 
 done 
 
