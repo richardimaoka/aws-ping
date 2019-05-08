2ef49902264da5e0c453a1b770ea1bd066f1336c
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-05-08 11:18:50 +0900

    send-command

---
+++ send-command.sh
@@ -0,0 +1,16 @@
+
+echo "Running a remote command to start the iperf server on ${IPERF_SERVER_INSTANCE_ID}"
+
+PING_ORIGIN_EC2=$(aws ec2 aws ec2 describe-instances  \
+ --filters "Name=tag:aws:cloudformation:stack-name,Values=PingExperiment" \
+ --query "Reservations[].Instances[].InstanceId" \
+ --text
+)
+
+aws ssm send-command \
+  --instance-ids "${IPERF_SERVER_INSTANCE_ID}" \
+  --document-name "AWS-RunShellScript" \
+  --comment "aws-ping command to run ping to all relevant EC2 instances in all the regions" \
+  --parameters commands=["/home/ec2-user/aws-ping/ping-all.sh"] \
+  --output text \
+  --query "Command.CommandId"
