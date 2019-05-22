0aaa2665006f7432405a06e98093c2029b085895
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-05-23 02:02:17 +0900

    missing whitespace

--- ping-script/icmp_line.sh
+++ ping-script/icmp_line.sh
@@ -53,7 +53,7 @@ else
   SECOND_HALF_THIRD_PART=$(echo "${SECOND_HALF}"  | awk '{print $3}') # (e.g.) "time=98.2"
   SECOND_HALF_FOURTH_PART=$(echo "${SECOND_HALF}"  | awk '{print $4}') # (e.g.) "ms"
 
-  if [ -z "$("${SECOND_HALF_FIRST_PART}" | awk '/^icmp_seq=[0-9]+$/')" ] ; then
+  if [ -z "$("${SECOND_HALF_FIRST_PART}" | awk '/^\sicmp_seq=[0-9]+$/')" ] ; then
     >&2 echo "ERROR: '${SECOND_HALF_FIRST_PART}' is not in the form of 'icmp_seq=*'"
     >&2 echo 
     exit 1
