49fadca816430cf2c581b4a39981605afd961541
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-06-09 01:55:12 +0900

    fix templ

--- create-stack.sh
+++ create-stack.sh
@@ -44,7 +44,7 @@ if [ -n "${ERROR}" ] ; then
 fi
 
 ######################################
-# 2 Create a CloudFormation stack
+# 2. Create a CloudFormation stack
 ######################################
 AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
 SSH_LOCATION="$(curl ifconfig.co 2> /dev/null)/32"
@@ -60,18 +60,23 @@ if ! STACK_INFO=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}
                   ParameterKey=AWSAccountId,ParameterValue="${AWS_ACCOUNT_ID}" \
     --region "${REGION}"
   )
-elif [ "CREATE_COMPLETED" != "$(echo "${STACK_INFO}" | jq -r '.Stacks[].StackStatus')" ] ; then
-  echo "Waiting until the CloudFormation stack is CREATE_COMPLETE for ${REGION}"
-  if ! aws cloudformation wait stack-create-complete --stack-name "${STACK_NAME}" --region "${REGION}"; then
-    >&2 echo "ERROR: CloudFormation wait failed for ${REGION}"
-    exit 1
-  fi
 else
   echo "Cloudformatoin stack in ${REGION} already exists"
 fi
 
+#######################################################
+# 3. Wait until the CloudFormation stack is ready
+#######################################################
+for REGION in ${REGIONS}
+do 
+  echo "Waiting until the CloudFormation stack is CREATE_COMPLETE for ${REGION}"
+  if ! aws cloudformation wait stack-create-complete --stack-name "${STACK_NAME}" --region "${REGION}"; then
+    >&2 echo "ERROR: CloudFormation wait failed for ${REGION}"
+    exit 1
+  fi    
+
 ###############################################
-# 2 Create a Subnet for each Availability Zone
+# 4 Create a Subnet for each Availability Zone
 ###############################################
 
 SUBNETS=$(aws ec2 describe-subnets \
